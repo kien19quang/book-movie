681816b891a52ba34ac8a513362627be86a13928
kien19quang <kien19quang@gmail.com>
2023-10-10 08:14:21 +0700

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @next/next/no-sync-scripts */
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 import '../styles/navbar.css';
@@ -37,7 +36,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" />
 
         {/* <!-- Bootstrap JS --> */}
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" async/>
 
         {/* <!-- Google Fonts --> */}
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet" />
