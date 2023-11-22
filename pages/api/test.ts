ce6098ebfc828233c3aedcb45063663ccae5a4a9
kien19quang <kien19quang@gmail.com>
2023-11-22 11:42:17 +0700

    update

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -8,7 +8,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         { value: 29, label: "Hà Nội" },
         { value: 36, label: "Thanh Hoá" },
         { value: 88, label: "Vĩnh Phúc" },
-        { value: 22, label: "Tuyên Quang" },
+        // { value: 22, label: "Tuyên Quang" },
       ]
 
       return res.status(200).json({ data: result })
